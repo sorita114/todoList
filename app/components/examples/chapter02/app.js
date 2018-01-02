24495513df96af132cdcb4ff3fbf3874605d7e8d
sorita114 <sorita114@gmail.com>
2018-01-02 17:05:43 +0900

    api 연결

--- app/components/examples/chapter02/app.js
+++ app/components/examples/chapter02/app.js
@@ -1,20 +1,42 @@
 import Vue from 'vue';
+import axios from 'axios';
 
 const App = new Vue({
 	el: '#app',
-	data: {
-		list: [{
-			id: 1,
-			text: 'test'
-		}, {
-			id: 2,
-			text: 'test'
-		}, {
-			id: 3,
-			text: 'test'
-		}, {
-			id: 4,
-			text: 'test'
-		}]
+	data : {
+		todos : [],
+		newTodo : ''
+	},
+	created () {
+		getTodo();
+	},
+	methods : {
+		add () {
+			addTodo(this, {text: this.newTodo});
+		}
 	}
 });
+
+function addTodo(target, data) {
+	axios
+		.post('http://localhost:8080/v1/todo', data)
+		.then(() => {
+			target.newTodo = '';
+
+			let todos = [data, ...target.todos];
+
+			target.todos = todos;
+		});
+}
+
+function getTodo() {
+	axios
+		.get('http://localhost:8080/v1/todo', {
+			headers : {
+				'Access-Control-Allow-Origin' : '*'
+			}
+		})
+		.then((response) => {
+			App.$set(App._data, 'todos', response.data._embedded.todos);
+		});
+}
